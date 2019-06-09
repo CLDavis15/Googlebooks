4ad647f57d068f397a0253c1a44390e85968205b
CLDavis15 <calebldavis0@gmail.com>
2019-06-09 13:58:15 -0400

    Making changes to search.js.

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,15 +4,13 @@ import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Search extends Component {
   state = {
     books: [],
     title: "",
-    author: "",
-    synopsis: ""
+    
   };
 
   componentDidMount() {
@@ -22,13 +20,13 @@ class Search extends Component {
   loadBooks = () => {
     API.getBooks()
       .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+        this.setState({ books: res.data, title: "", author: "", description: "", image: "", link: "" })
       )
       .catch(err => console.log(err));
   };
 
-  deleteBook = id => {
-    API.deleteBook(id)
+  deleteBook = _id => {
+    API.deleteBook(_id)
       .then(res => this.loadBooks())
       .catch(err => console.log(err));
   };
@@ -40,17 +38,13 @@ class Search extends Component {
     });
   };
 
-  handleFormSubmit = event => {
+  handleSearchSubmit = event => {
     event.preventDefault();
-    if (this.state.title && this.state.author) {
-      API.saveBook({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
-      })
+      API.searchBooks(
+        this.state.books
+      )
         .then(res => this.loadBooks())
         .catch(err => console.log(err));
-    }
   };
 
   render() {
@@ -68,46 +62,13 @@ class Search extends Component {
                 name="title"
                 placeholder="Title (required)"
               />
-              <Input
-                value={this.state.author}
-                onChange={this.handleInputChange}
-                name="author"
-                placeholder="Author (required)"
-              />
-              <TextArea
-                value={this.state.synopsis}
-                onChange={this.handleInputChange}
-                name="synopsis"
-                placeholder="Synopsis (Optional)"
-              />
               <FormBtn
-                disabled={!(this.state.author && this.state.title)}
+                disabled={!(this.state.title)}
                 onClick={this.handleFormSubmit}
               >
                 Submit Book
               </FormBtn>
             </form>
-          </Col>
-          <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {this.state.books.length ? (
-              <List>
-                {this.state.books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
           </Col>
         </Row>
       </Container>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,15 +6,15 @@ export default {
     return axios.get("/api/books");
   },
   // Gets the book with the given id
-  searchBook: (_id) => {
+  searchBooks: (_id) => {
     return axios.get("/search" + _id);
   },
   // Deletes the book with the given id
-  deleteBook: (_id) => {
+  deleteBooks: (_id) => {
     return axios.delete("/api/books/" + _id);
   },
   // Saves a book to the database
-  addBook: (bookData) => {
+  addBooks: (bookData) => {
     return axios.post("/api/books", bookData);
   }
 };
