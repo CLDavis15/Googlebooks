33f216663865c26a5a3e934dc4d2b6e334b7a09b
CLDavis15 <calebldavis0@gmail.com>
2019-06-17 18:59:56 -0400

    setup google api routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const googleRoutes = requrie("./google");
 
 // Book routes
 router.use("/books", bookRoutes);
+
+router.use("/google", googleRoutes);
 
 module.exports = router;
